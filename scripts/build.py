d52258b90d4d6814d27d578bd6369a4c8941c21e
rim <18486012+0RIM0@users.noreply.github.com>
2026-04-19 14:34:57 +0900

    sw のパスを絶対パス参照に

--- scripts/build.py
+++ scripts/build.py
@@ -1,3 +1,17 @@
 import sys
+from pathlib import Path
 
-print(sys.argv)
+def replace_file(filepath, replace_dict):
+	path = Path(filepath)
+
+	if not path.is_file():
+		raise Exception(f"{file_path} がファイルではありません")
+
+	content = path.read_text(encoding="utf-8")
+
+	for src, dst in replace_dict.items():
+		content = content.replace(src, dst)
+
+	path.write_text(content, encoding="utf-8")
+
+replace_file("public/404.html", { "{{deploy_root}}": sys.arv[1] })
